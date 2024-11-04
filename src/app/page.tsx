69bd82418f46fe7bff8c22b2f6ac36a70e3ed0fe
HITMAN <112652819+ayushpatil2122@users.noreply.github.com>
2024-11-04 19:12:38 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import  MyTable  from "@/components/table/MyTable";
 export default function Home() {
   return (
     <>
+      <script src="http://localhost:3000/api/products/a21c217d-2028-4656-8e7f-95c70c0156e2/banner"></script>
       <MyTable />
     </>
   );
