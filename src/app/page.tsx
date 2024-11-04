4179b2f63655fa92dd0147c68e5a4a414f4c06ef
HITMAN <112652819+ayushpatil2122@users.noreply.github.com>
2024-11-04 19:24:46 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import  MyTable  from "@/components/table/MyTable";
 export default function Home() {
   return (
     <>
-      hii
         <script src="https://cdn.paritydeals.com/banner.js"></script>
       <MyTable />
     </>
